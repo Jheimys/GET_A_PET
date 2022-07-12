b3402c44728931cff65d20d5fc841fe797ec6fe2
Jheimys Bassani <jheimysbb@gmail.com>
2022-07-12 14:18:04 -0300

    Pegando token do header

--- backend/helpers/create-user-token.js
+++ backend/helpers/create-user-token.js
@@ -2,10 +2,14 @@ const jwt = require('jsonwebtoken')
 
 const createUserToken = async (user, req, res) => {
     //create token
-    const token = jwt.sign({
-        name: user.name,
-        id: user._id,
-    }, "nossosecret")
+    const token = jwt.sign(
+        {
+            name: user.name,
+            id: user._id,
+        }, 
+
+        "nossosecret"
+    )
 
     //return token
     res.status(200).json({
